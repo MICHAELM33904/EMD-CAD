86e22ad09a4deb0844dd839adb94b17a964d54f4
Michael Martin <michael@smartinrealestate.net>
2025-04-13 04:47:24 -0400

    Initial clean deploy of EMD CAD

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to EMD CAD</h1>
+      <p>This is your working app. Full features will be restored once connected to GitHub and Vercel correctly.</p>
+    </div>
+  );
+}
+
+export default App;
